0d4bb344181c61d08cf469e024599c843828fe82
Marie Jossé <marie14.josse@gmail.com>
2022-06-06 22:14:31 +0200

    finalize preproces

--- test_planemo/preprocess_S2.r
+++ test_planemo/preprocess_S2.r
@@ -70,17 +70,16 @@ S2obj <- extract_from_S2_L2A(Path_dir_S2 = Path_S2,
 # update shapefile if needed (reprojection)
 ###path_vector <- S2obj$path_vector
 
-write.csv(S2source, "truc.csv")
 # create specific result directory corresponding to granule name
-results_site_path <- file.path(result_path,basename(S2obj$S2_Bands$GRANULE))
-dir.create(path = results_site_path,showWarnings = FALSE,recursive = TRUE)
+#results_site_path <- file.path(result_path,basename(S2obj$S2_Bands$GRANULE))
+#dir.create(path = results_site_path, showWarnings = FALSE, recursive = TRUE)
 ##____________________________________________________________________##
 ##                        Write CLOUD MASK                            ##
 ##--------------------------------------------------------------------##
 
 # directory for cloud mask
-Cloud_path <- file.path(results_site_path,'CloudMask')
-dir.create(path = Cloud_path,showWarnings = FALSE,recursive = TRUE)
+Cloud_path <- file.path(result_path,'CloudMask')
+dir.create(path = Cloud_path, showWarnings = FALSE, recursive = TRUE)
 # Filename for cloud mask
 cloudmasks <- save_cloud_s2(S2_stars = S2obj$S2_Stack,
                                        Cloud_path = Cloud_path,
@@ -91,10 +90,10 @@ cloudmasks <- save_cloud_s2(S2_stars = S2obj$S2_Stack,
 ##--------------------------------------------------------------------##
  
 # directory for Reflectance
-Refl_dir <- file.path(results_site_path,'Reflectance')
-dir.create(path = Refl_dir,showWarnings = FALSE,recursive = TRUE)
+Refl_dir <- file.path(result_path,'Reflectance')
+dir.create(path = Refl_dir, showWarnings = FALSE, recursive = TRUE)
 # filename for Reflectance
-Refl_path <- file.path(Refl_dir, paste(basename(S2obj$S2_Bands$GRANULE), 'Refl_', sep = ''))
+Refl_path <- file.path(Refl_dir, paste(basename(S2obj$S2_Bands$GRANULE), '_Refl', sep = ''))
 
 # Save Reflectance file as ENVI image with BIL interleaves
 tile_S2 <- substring(strsplit(basename(S2obj$S2_Bands$GRANULE),'_')[[1]][2],2)
@@ -103,5 +102,6 @@ dateAcq_S2 <- as.Date(substring(strsplit(basename(S2obj$S2_Bands$GRANULE),'_')[[
 save_data <- save_reflectance_s2(S2_stars = S2obj$S2_Stack, Refl_path = Refl_path,
                                S2Sat = NULL, tile_S2 = tile_S2, dateAcq_S2 = dateAcq_S2,
                                Format = 'ENVI', datatype = 'Int16', MTD = S2obj$S2_Bands$metadata, MTD_MSI = S2obj$S2_Bands$metadata_MSI)
+
 
 
